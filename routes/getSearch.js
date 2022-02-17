544cd936d326ac77eb1258fab3c46f676cac9f76
plaeschs <Sascha.zierke@outlook.com>
2022-02-17 10:51:24 +0100

    searchObject route

--- routes/getSearch.js
+++ routes/getSearch.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 router.post('/', (req, res) => {
     console.log(req.body)
-    let data = JSON.stringify(req.body, 2)
+    let data = JSON.stringify(req.body,null, 2)
     fs.writeFileSync('searchResult.json', data);
     res.send("okay")
 })
