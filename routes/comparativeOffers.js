16d9c8fd530f411a48522992715952caea556b5f
ShinyKampfkeule <kevleh95@googlemail.com>
2022-02-19 14:00:01 +0100

    Changed things

--- routes/comparativeOffers.js
+++ routes/comparativeOffers.js
@@ -1,27 +1,46 @@
 const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch')
-const jsonFile = require('../searchResult.json')
-
-const result = jsonFile.searchObject
-
+const fs = require('fs')
 
 router.post('/', async function(req, res) {
     try {
-        fetch('https://api.21re.de/v3/comparables/query', {
-            method: 'POST',
-            headers: {
-                'accept': 'application/json',
-                'content-type': 'application/json',
-                'authorization': 'Bearer FP14A204OJQ3TPUQ8A1GA4EFV6I1NRV8S50ODCT2IRO15JPA7M8GCB1R',
-                'Content-Type': 'application/json; charset=UTF-8'
-            },
-            body: JSON.stringify({"address":{"country":`${result.context[3].short_code.toUpperCase()}`,"locality":`${result.context[2].text}`,"postalCode":`${result.context[0].text}`,"route":`${result.text}`,"streetNumber":`${result.address}`,"location":{"lat":52.51235,"lng":13.39734}},"radius":1000,"offerType":"rent","objectType":"apartment","area":{"from":40,"to":90},"year":{"from":1980,"to":2000},"rooms":{"from":2,"to":5},"offerDate":{"from":{"year":2010,"quarter":1},"to":{"year":2010,"quarter":1}}})
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                res.json({data: data})
+        let result
+        await fs.readFile('searchResult.json', (err, data) => {
+            if (err) throw err;
+            let json = JSON.parse(data);
+            result = json.searchObject
+            fetch('https://api.21re.de/v3/comparables/query', {
+                method: 'POST',
+                headers: {
+                    'accept': 'application/json',
+                    'content-type': 'application/json',
+                    'authorization': 'Bearer FP14A204OJQ3TPUQ8A1GA4EFV6I1NRV8S50ODCT2IRO15JPA7M8GCB1R',
+                    'Content-Type': 'application/json; charset=UTF-8'
+                },
+                body: JSON.stringify({
+                    "address": {
+                        "country": `${result.context[3].short_code.toUpperCase()}`,
+                        "locality": `${result.context[2].text}`,
+                        "postalCode": `${result.context[0].text}`,
+                        "route": `${result.text}`,
+                        "streetNumber": `${result.address}`,
+                        "location": {"lat": result.center[1], "lng": result.center[0]},
+                    },
+                    "radius": 50000,
+                    "offerType": "rent",
+                    "objectType": "apartment",
+                    "area": {"from": 40, "to": 90},
+                    "year": {"from": 1980, "to": 2000},
+                    "rooms": {"from": 2, "to": 5},
+                    "offerDate": {"from": {"year": 2010, "quarter": 1}, "to": {"year": 2010, "quarter": 1}}
+                })
             })
+                .then((response) => response.json())
+                .then((data) => {
+                    res.json({data: data})
+                })
+        })
     } catch (e) {
     }
 });
--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,45 @@
 const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch')
-const jsonFile = require('../searchResult.json')
-
-const result = jsonFile.searchObject
-const makro = jsonFile.makro
-const mikro = jsonFile.mikro
+const fs = require('fs')
 
 router.post('/', async function(req, res) {
   try {
-    fetch('https://api.21re.de/v3/scores', {
-      method: 'POST',
-      headers: {
-        'accept': 'application/json',
-        'content-type': 'application/json',
-        'authorization': 'Bearer FP14A204OJQ3TPUQ8A1GA4EFV6I1NRV8S50ODCT2IRO15JPA7M8GCB1R',
-        'Content-Type': 'application/json; charset=UTF-8'
-      },
-      body: JSON.stringify({"address":{"country":`${result.context[3].short_code.toUpperCase()}`,"locality":`${result.context[2].text}`,"postalCode":`${result.context[0].text}`,"route":`${result.text}`,"streetNumber":`${result.address}`,"location":{"lat":52.51235,"lng":13.39734}},"macro":[`${makro[0].name}`, `${makro[1].name}`, `${makro[2].name}`, `${makro[3].name}`],"micro":[`${mikro[0].name}`, `${mikro[1].name}`, `${mikro[2].name}`, `${mikro[3].name}`]})
-    })
-        .then((response) => response.json())
-        .then((data) => {
-          res.json({data: data, makro: makro, mikro: mikro})
+    let result
+    let makro
+    let mikro
+    await fs.readFile('searchResult.json', (err, data) => {
+      if (err) throw err;
+      let json = JSON.parse(data);
+      result = json.searchObject
+      makro = json.makro
+      mikro = json.mikro
+      fetch('https://api.21re.de/v3/scores', {
+        method: 'POST',
+        headers: {
+          'accept': 'application/json',
+          'content-type': 'application/json',
+          'authorization': 'Bearer FP14A204OJQ3TPUQ8A1GA4EFV6I1NRV8S50ODCT2IRO15JPA7M8GCB1R',
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: JSON.stringify({
+          "address": {
+            "country": `${result.context[3].short_code.toUpperCase()}`,
+            "locality": `${result.context[2].text}`,
+            "postalCode": `${result.context[0].text}`,
+            "route": `${result.text}`,
+            "streetNumber": `${result.address}`,
+            "location": {"lat": result.center[1], "lng": result.center[0]},
+          },
+          "macro": [`${makro[0].name}`, `${makro[1].name}`, `${makro[2].name}`, `${makro[3].name}`],
+          "micro": [`${mikro[0].name}`, `${mikro[1].name}`, `${mikro[2].name}`, `${mikro[3].name}`]
         })
+      })
+          .then((response) => response.json())
+          .then((data) => {
+            res.json({data: data, makro: makro, mikro: mikro})
+          })
+    })
   } catch (e) {
   }
 });
