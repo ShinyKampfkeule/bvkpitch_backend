b1800b5481f1cfde8787a1092d53554dbeffd152
ShinyKampfkeule <kevleh95@googlemail.com>
2022-02-21 13:52:23 +0100

    Changed things

--- routes/getSearch.js
+++ routes/getSearch.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const fs = require('fs');
 
 router.post('/', (req, res) => {
-    let data = JSON.stringify(req.body,null, 2)
-    fs.writeFileSync('searchResult.json', data);
-    res.send("okay")
+    if ((req.body.makro.length < 4) || (req.body.mikro.length < 4)) {
+        res.send({message: "error"})
+    } else {
+        let data = JSON.stringify(req.body,null, 2)
+        fs.writeFileSync('searchResult.json', data);
+        res.send({message: "okay"})
+    }
 })
 
 module.exports = router;
